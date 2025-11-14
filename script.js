27bd0641a2106af258b49f5f6d41a892de4c1f6c
Linda K <kazusalinda@gmail.com>
2025-11-14 15:45:33 +0200

    js-finished

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let activities = [];
 let dataDaily = {};
 let dataWeekly = {};
 let dataMonthly = {};
+
+const timeFrames = document.querySelector(".user-timeframes");
+const btnDaily = document.querySelector(".btn-daily");
+const btnWeekly = document.querySelector(".btn-weekly");
+const btnMonthly = document.querySelector(".btn-monthly");
+
 const currentTimingElements = document.querySelectorAll(".card-body h3");
 const previousTimingElements = document.querySelectorAll(".card-body p");
 
@@ -23,54 +29,46 @@ fetch("data.json")
       dataMonthly[activity.title] = activity.timeframes.monthly;
     });
 
-    console.log("Activities loaded:", activities);
-    console.log("Daily data:", dataDaily);
-    console.log("Weekly data:", dataWeekly);
-    console.log("Monthly data:", dataMonthly);
-
     // Set up buttons
     setupButtons();
   })
   .catch((error) => console.error("Error loading data:", error));
 
+//Updating UI
+function updateUI(data) {
+  //updating current time:
+  currentTimingElements.forEach((el) => {
+    const key = el.dataset.name; //reading data-key
+    //if data-key is the same as object's key, change innerHTML
+    if (data[key]) {
+      el.innerHTML = `${data[key].current} hrs`;
+    }
+  });
+  //updating previous week:
+  previousTimingElements.forEach((el) => {
+    const key = el.dataset.name;
+    if (data[key]) {
+      el.innerHTML = `Last week - ${data[key].previous} hrs`;
+    }
+  });
+}
 // Button setup
 function setupButtons() {
-  const btnDaily = document.querySelector(".btn-daily");
-  const btnWeekly = document.querySelector(".btn-weekly");
-  const btnMonthly = document.querySelector(".btn-monthly");
-
-  btnDaily.addEventListener("click", () => {
-    currentTimingElements.forEach((el) => {
-      const key = el.dataset.name; //reading data-key
-      //if data-key is the same as object's key, change innerHTML
-      if (dataDaily[key]) {
-        el.innerHTML = `${dataDaily[key].current} hrs`;
-      }
-    });
-    previousTimingElements.forEach((el) => {
-      const key = el.dataset.name;
-      if (dataDaily[key]) {
-        el.innerHTML = `Last week - ${dataDaily[key].previous} hrs`;
-      }
-    });
-  });
-
-  btnWeekly.addEventListener("click", () => {
-    console.log("Weekly Data:", dataWeekly);
-    console.log("Work weekly:", dataWeekly["Work"]);
-  });
-
-  btnMonthly.addEventListener("click", () => {
-    console.log("Monthly Data:", dataMonthly);
-    console.log("Work Monthly:", dataMonthly["Work"]);
+  timeFrames.addEventListener("click", function (e) {
+    if (e.target === btnDaily) {
+      updateUI(dataDaily);
+    } else if (e.target === btnWeekly) {
+      updateUI(dataWeekly);
+    } else if (e.target === btnMonthly) {
+      updateUI(dataMonthly);
+    }
   });
 }
 
-// const headings = document.querySelectorAll(".card-body h3");
+//TO DO:
 
-// headings.forEach((h3) => {
-//   const key = h3.dataset.key; // reads data-key
-//   if (data.daily[key]) {
-//     h3.innerHTML = data.daily[key].current; // set content
-//   }
-// });
+//Mobile design, media queries
+//Go through JS once more to remember stuff
+//Modify read me
+//Deploy to github, netlify
+//Upload
